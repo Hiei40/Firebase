f5701cc6d20fb2d1810a608e47a5ba2ece290864
Mohamed Khaled <k.abdalrhem@gmail.com>
2024-01-14 13:58:21 +0200

    add

--- lib/Catigories/Add.dart
+++ lib/Catigories/Add.dart
@@ -1,3 +1,5 @@
+import 'package:firebase/Components/CustomButtonAuth.dart';
+import 'package:firebase/Components/TextFormField.dart';
 import 'package:flutter/material.dart';
 
 class AddCategory extends StatefulWidget {
@@ -8,11 +10,34 @@ class AddCategory extends StatefulWidget {
 }
 
 class _AddCategoryState extends State<AddCategory> {
+  GlobalKey<FormState>globalKey=GlobalKey<FormState>();
+TextEditingController name=TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("addcategory"),
+      ),
+      body: Form(
+        key: globalKey, child:Column(
+        children: [
+
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0,
+              horizontal: 25 ),
+              child: CustomTextForm(hinttext: "ادخل", mycontroller: name, validator: (String?Value){
+                if(Value==""){
+                  return"can\' t be empty ";
+                }
+
+              }),
+            ),
+          ),
+         Custombuttonauth(onPressed: (){}, Title: "add")
+        ],
+      ),
+
 
       ),
 
