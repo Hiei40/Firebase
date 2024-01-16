3b716111b3601a407673840a514da59f6a3f65ea
Mohamed Khaled <k.abdalrhem@gmail.com>
2024-01-16 22:03:37 +0200

    add

--- lib/Catigories/Add.dart
+++ lib/Catigories/Add.dart
@@ -4,64 +4,70 @@ import 'package:firebase/Components/TextFormField.dart';
 import 'package:flutter/material.dart';
 
 class AddCategory extends StatefulWidget {
-  const AddCategory({super.key});
+  const AddCategory({Key? key}) : super(key: key);
 
   @override
   State<AddCategory> createState() => _AddCategoryState();
 }
 
 class _AddCategoryState extends State<AddCategory> {
-  GlobalKey<FormState>formState=GlobalKey<FormState>();
-TextEditingController name=TextEditingController();
-  CollectionReference categories = FirebaseFirestore.instance.collection('categories');
+  final GlobalKey<FormState> formState = GlobalKey<FormState>();
 
-addUser() async{
-    if(formState.currentState!.validate()){
-      try{
-        DocumentReference response =await categories.add({"name": name.text});
-        Navigator.of(context).pushReplacementNamed("homepage");
+  TextEditingController nameController = TextEditingController(); // Move the controller outside the method
+
+  addUser() async {
+    if (formState.currentState!.validate()) {
+      try {
+        DocumentReference response = await FirebaseFirestore.instance.collection('categories').add({
+          'name': nameController.text,
+        });
+
+        Navigator.of(context).pushReplacementNamed('homePage');
         print(response);
-      }catch(e){
-        print("Error $e");
+      } catch (e) {
+        print('Error $e');
       }
-
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("addcategory"),
+        title: Text('Add Category'),
       ),
       body: Form(
-        key: formState, child:Column(
-        children: [
-
-          Container(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20.0,
-              horizontal: 25 ),
-              child: CustomTextForm(hinttext: "Enter Name", mycontroller: name, validator: (String?Value){
-                if(Value==""){
-                  return"can\' t be empty ";
-                }
-                else
-                  {
-                    return null;
-                  }
-
-              }),
+        key: formState,
+        child: Column(
+          children: [
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 20.0,
+                  horizontal: 25,
+                ),
+                child: TextFormField(
+                  controller: nameController, // Use the controller for the TextFormField
+                  decoration: InputDecoration(
+                    hintText: 'Enter Name',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Can't be empty";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+              ),
             ),
-          ),
-         Custombuttonauth(onPressed: (){
-           addUser();
-         }, Title: "add")
-        ],
+            Custombuttonauth(
+              onPressed: addUser,
+             Title: 'Add',
+            ),
+          ],
+        ),
       ),
-
-
-      ),
-
     );
   }
 }
--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,72 +1,91 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'Auth/Login.dart';
-import 'Catigories/Add.dart';
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<QueryDocumentSnapshot> data = [];
+
+  getData() async {
+    QuerySnapshot querySnapshot =
+    await FirebaseFirestore.instance.collection("categories").get();
+    data = querySnapshot.docs; // Use direct assignment instead of addAll
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const AddCategory()),
-          );        },
+        onPressed: () {
+          Navigator.of(context).pushReplacementNamed("addcategory");
+        },
         backgroundColor: Colors.orange,
         child: Icon(Icons.add),
-
       ),
       appBar: AppBar(
         title: Text("FireBase"),
         actions: [
           IconButton(
             onPressed: () async {
-              GoogleSignIn googlesignIn = GoogleSignIn();
-              googlesignIn.disconnect();
+              GoogleSignIn googleSignIn = GoogleSignIn();
+              await googleSignIn.disconnect();
 
               await FirebaseAuth.instance.signOut();
               Navigator.of(context).pushAndRemoveUntil(
                 MaterialPageRoute(builder: (context) => Login()),
-                (Route<dynamic> route) => false,
+                    (Route<dynamic> route) => false,
               );
             },
             icon: Icon(Icons.exit_to_app),
           )
         ],
       ),
-      body: GridView(
+      body: GridView.builder(
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, mainAxisExtent: 250),
-        children: [
-          Card(
+          crossAxisCount: 2,
+          mainAxisExtent: 250,
+        ),
+        itemCount: data.length,
+        itemBuilder: (BuildContext context, int index) {
+          Map<String, dynamic> categoryData =
+          data[index].data() as Map<String, dynamic>;
+
+          // Make sure to check if 'categories' and 'name' fields exist
+          String categoryName = categoryData.containsKey('name')
+              ? categoryData['name'].toString()
+              : 'No Category';
+
+          return Card(
             child: Column(
               children: [
                 Container(
-                    child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Image.asset(
-                      "Image/kisspng-emoji-file-folders-directory-computer-icons-txt-file-5acd8ad8c2a790.3510068315234198647973.png"),
-                )),
-                Text("Company")
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Image.asset(
+                      "Image/kisspng-emoji-file-folders-directory-computer-icons-txt-file-5acd8ad8c2a790.3510068315234198647973.png",
+                    ),
+                  ),
+                ),
+                Text(categoryName),
               ],
             ),
-          ),
-          Card(
-            child: Column(
-              children: [
-                Container(
-                    child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Image.asset(
-                      "Image/kisspng-emoji-file-folders-directory-computer-icons-txt-file-5acd8ad8c2a790.3510068315234198647973.png"),
-                )),
-                Text("Home")
-              ],
-            ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
