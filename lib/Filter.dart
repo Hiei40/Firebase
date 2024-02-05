f29220cd428a0a92b7fd0e780b0167d6eb8a03e7
Mohamed Khaled <k.abdalrhem@gmail.com>
2024-02-05 09:34:27 +0200

    ImagePicker finish

--- lib/Filter.dart
+++ lib/Filter.dart
@@ -3,7 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-
+import 'package:path/path.dart';
 class FilterFirestore extends StatefulWidget {
   const FilterFirestore({Key? key}) : super(key: key);
 
@@ -15,17 +15,21 @@ class _FilterFirestoreState extends State<FilterFirestore> {
   final Stream<QuerySnapshot> usersStream =
       FirebaseFirestore.instance.collection('users').snapshots();
   File? file;
+  String? url;
+
 
   getImage() async {
     final ImagePicker picker = ImagePicker();
     final XFile? photo = await picker.pickImage(source: ImageSource.camera);
 
-    if (photo != null) {
-      file = File(photo.path);
-      var refStorage = FirebaseStorage.instance.ref("1.jpg");
+
+      file = File(photo!.path);
+      var imagename=basename(photo.path);
+      var refStorage = FirebaseStorage.instance.ref(imagename);
       await refStorage.putFile(file!); // This line was missing in your code
+   url=await refStorage.getDownloadURL();
       setState(() {});
-    }
+
   }
 
   @override
@@ -43,9 +47,9 @@ class _FilterFirestoreState extends State<FilterFirestore> {
               },
               child: Text("Get Image Camera"),
             ),
-            if (file != null)
-              Image.file(
-                file!,
+            if (url != null)
+              Image.network(
+                url!,
                 width: 100,
                 height: 100,
                 fit: BoxFit.fill,
